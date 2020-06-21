e6d17488723e2c018a245124e251c9a2214b71e4
kenji <tora169mm@gmail.com>
2020-06-21 11:38:05 +0900

    setup SPA and vue router

--- web/routes/web.php
+++ web/routes/web.php
@@ -11,10 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// APIのURL以外のリクエストに対してはindexテンプレートを返す
+// 画面遷移はフロントエンドのVueRouterが制御する
+Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
 
-Auth::routes();
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
