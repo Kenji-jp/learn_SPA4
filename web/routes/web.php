97b8b6cbbf9da871db4477ca22dabef6610f2c84
kenji <tora169mm@gmail.com>
2020-06-21 15:20:59 +0900

    update config.yml

--- web/routes/web.php
+++ web/routes/web.php
@@ -15,10 +15,10 @@
 // 画面遷移はフロントエンドのVueRouterが制御する
 // Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Auth::routes();
+Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
