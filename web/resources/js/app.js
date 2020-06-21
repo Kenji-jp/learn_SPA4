84c99cf28e5e255458dfaa0734c81d6501026bde
kenji <tora169mm@gmail.com>
2020-06-22 07:57:37 +0900

    [add] add vuex

--- web/resources/js/app.js
+++ web/resources/js/app.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 // ルーティングの定義をインポートする
 import router from './router'
+// ストアをインポートする
+import store from './store' // add
 // ルートコンポーネントをインポートする
 import App from './App.vue'
+// ブートストラップをインポートする
+import './bootstrap'
 
 new Vue({
   el: '#app',
   router, // ルーティングの定義を読み込む
+  store,  // ストアを読み込む
   components: { App }, // ルートコンポーネントの使用を宣言する
   template: '<App />' // ルートコンポーネントを描画する
 })
--- web/routes/web.php
+++ web/routes/web.php
@@ -13,7 +13,7 @@
 
 // APIのURL以外のリクエストに対してはindexテンプレートを返す
 // 画面遷移はフロントエンドのVueRouterが制御する
-// Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
+Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
 
 // Route::get('/', function () {
 //     return view('welcome');
