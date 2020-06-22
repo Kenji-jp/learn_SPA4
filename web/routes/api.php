57575e1f430d47418492ac19f96199cd87ccc848
kenji <tora169mm@gmail.com>
2020-06-22 09:11:42 +0900

    [fix] make test using vuex

--- web/routes/api.php
+++ web/routes/api.php
@@ -13,3 +13,18 @@ Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 
 // login user
 Route::get('/user', fn() => Auth::user())->name('user');
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
